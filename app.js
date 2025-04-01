368c8948137d88ab35c68aca6cfaac715b010e9f
Sigit <132531211+bulanterbit@users.noreply.github.com>
2025-04-01 13:43:34 +0700

    inisiasi pertama

---
+++ app.js
@@ -0,0 +1,42 @@
+import express from "express";
+
+import { PORT } from "./config/env.js";
+
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
+
+import errorMiddleware from "./middleware/error.middleware.js";
+import arcjetMiddleware from "./middleware/arcjet.middleware.js";
+
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(arcjetMiddleware);
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
+
+// error handling middleware
+app.use(errorMiddleware);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the subscription Tracker API");
+});
+
+app.listen(PORT, async () => {
+  console.log(
+    `Subscription Tracker API is running on http://localhost:${PORT}`
+  );
+
+  await connectToDatabase();
+});
+
+export default app;
